9ad038d0c94259885d0661d86918abdc9477234b
DESKTOP-M0MQKKV\Administrator <levelhoon@gmail.com>
2022-04-20 02:31:07 +0900

    no message

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,7 @@
 import GetUser from '../components/GetUser';
 import LoginButton from '../components/LoginButton'
 
+//로그인 페이지
 export default function Login(props) {
   GetUser();
   return <LoginButton className="loginButton" authURL={props.authURL}/>;

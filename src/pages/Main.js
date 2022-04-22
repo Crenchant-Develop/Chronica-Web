b71f8536917953e6d17178b9a844c8f0708c772f
DESKTOP-M0MQKKV\Administrator <levelhoon@gmail.com>
2022-04-23 08:29:51 +0900

    쿠키함수 모듈화

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,29 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import cookies from 'react-cookies';
 
 //defined components
 import { GetUserName } from '../components/GetUser';
+import { getCookie, setCookie } from '../components/Cookies';
 
-
-//코드 이해를 돕기위해 모듈화 안했음. 곧 모듈화 예정
-//쿠키저장
-function setCookie(key, value) {
-  var expires = new Date;
-  expires.setFullYear(expires.getFullYear() + 10);
-  cookies.save(key, value, {
-    path: '/', // 쿠키 값을 저장하는 서버 경로
-    expires, // 유효 시간
-    //secure: true,
-    // 웹 브라우저와 웹 서버가 https로 통신하는 경우에만 쿠키 저장
-    //httpOnly: true
-    // document.cookie라는 자바스크립트 코드로 쿠키에 비정상적으로 접속하는 것을 막는 옵션 
-  })
-}
-
-//쿠키가져오기
-function getCookie(cookieName) {
-  return cookies.load(cookieName);
-}
 
 //메인페이지
 function Main() {

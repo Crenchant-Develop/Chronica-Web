57bc86f2b438db2dd9a5487a8c94c2877b00386c
DESKTOP-M0MQKKV\Administrator <levelhoon@gmail.com>
2022-05-03 17:00:06 +0900

    mui footer적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BottomNavigation } from '@mui/material';
 
 export default function Footer(props) {
+  const style = {  
+    backgroundColor: '#202020',
+    boxShadow: '0 0 10px black',
+    width: '100%',
+    height: '5vw',
+  };
+
   return (
     <footer {...props}>
-        <div className="context">
-          what
-          <h5>Copyright &copy; 2022 Netea All Rights Reserved.</h5>
-        </div>
+      <BottomNavigation className="context" style={style}>
+        <h5 style={{color: '#606060'}}>Copyright &copy; 2022 Netea All Rights Reserved.</h5>
+      </BottomNavigation>
     </footer>
   );
 }
